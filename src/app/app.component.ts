2d6fd847aa3af4d2426d55b2518e35edfbe1b5f8
Baroslav <75853671+Baroslav@users.noreply.github.com>
2023-10-13 12:32:02 +0300

    depluy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password_app';
+  password = "";
+  requiredColor = ['grey','grey','grey'];
+  inputColor = ""
+  checkPasswordStrengh() {
+    const password : string = this.password;
+    const requiredLatter = /[a-zA-Z]/;
+    const requiredNumber = /[0-9]/;
+    const requiredSymbol = /[^A-Za-z0-9]/;
+
+    if(password.length === 0) {
+      this.requiredColor = ['grey','grey','grey'];
+    }else if(password.length < 8) {
+      this.requiredColor = ['red','red','red'];
+    }else if(
+      password.search(requiredLatter) !==-1 && 
+      password.search(requiredNumber) !==-1 && 
+      password.search(requiredSymbol) !==-1 
+      ) {
+      this.requiredColor = ['green','green','green']
+    }else if(
+      (password.search(requiredLatter) !==-1 && password.search(requiredNumber) !==-1) ||
+      (password.search(requiredNumber) !==-1 && password.search(requiredSymbol) !==-1) ||
+      (password.search(requiredLatter) !==-1 && password.search(requiredSymbol) !==-1)
+      ) {
+      this.requiredColor = ['yellow','yellow','red']
+    }else if(
+      password.search(requiredLatter) !==-1 ||
+      password.search(requiredNumber) !==-1 ||
+      password.search(requiredSymbol) !==-1 
+      ) {
+      this.requiredColor = ['red','grey','grey']
+    }
+  }
+
 }
